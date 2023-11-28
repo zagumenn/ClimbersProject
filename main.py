58516b9cb9eef758bcd5ffaf57e14390864b18e1
zagumenn <66692129+zagumenn@users.noreply.github.com>
2023-11-28 17:27:55 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from Models.Ascents import Ascents
 from Models.Climbers import Climbers
 from Models.Model import Model
 from Models.Mountains import Mountains
+from Models.Regons import Regons
 
 # climber = Model()
 # print(climber.get('Climbers'))
@@ -23,6 +25,8 @@ from Models.Mountains import Mountains
     # print(row, ' - ', list)
 
 # climber.update()
+
+# Методы для таблицы Mountains
 mountain = Mountains()
 print(mountain.get())
 
@@ -36,3 +40,33 @@ for row, list in enumerate(mountain.get()):
     print(row, ' - ', list)
 
 mountain.update()
+
+# Методы для таблицы Ascents
+ascent = Ascents()
+print(ascent.get())
+
+for row, list in enumerate(ascent.get()):
+    print(row, ': ', list)
+
+ascent.add()
+ascent.delete(1)
+
+for row, list in enumerate(ascent.get()):
+    print(row, ' - ', list)
+
+ascent.update()
+
+# Методы для таблицы Regons
+regon = Regons()
+print(regon.get())
+
+for row, list in enumerate(regon.get()):
+    print(row, ': ', list)
+
+regon.add()
+regon.delete(1)
+
+for row, list in enumerate(regon.get()):
+    print(row, ' - ', list)
+
+regon.update()
